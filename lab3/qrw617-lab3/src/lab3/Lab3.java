d57834cc9fcf3f7c86ab81ceaecf96e9c44a94d5
Jose <jbocanegra96@gmail.com>
2016-09-12 22:07:37 -0500

    first commit

---
+++ lab3/qrw617-lab3/src/lab3/Lab3.java
@@ -0,0 +1,31 @@
+package lab3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+//import java.util.Calendar;
+import java.util.Scanner;
+
+public class Lab3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = null;
+		try {
+		    in = new Scanner(new File("dates.txt"));
+		} catch (FileNotFoundException exception) {
+		    System.err.println("failed to open dates.txt");
+		    System.exit(1);
+		}
+		while (in.hasNextLine()) {
+		    String line = in.nextLine();
+			
+			Date date = new Date(line);
+		    System.out.println(date);
+		    // need more code for DateRange objects
+		}    
+		
+		//System.out.println(date);
+		//System.out.println(daterange);
+	}
+
+}
