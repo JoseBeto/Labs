97af95634370ec0671a1a4de9af55b7061887a20
U-GPMLIFE\CJB <CJB@CJBHP.gpmlife.com>
2016-09-13 14:37:53 -0500

    added author

--- qrw617-lab3/src/lab3/Lab3.java
+++ qrw617-lab3/src/lab3/Lab3.java
@@ -12,6 +12,10 @@ import java.util.Scanner;
  ******************************/
 
 public class Lab3 {
+	
+	/*
+	 * Main method comment
+	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
