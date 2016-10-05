aa359a7ff18ddf533978c36950e2a623fdb620a1
unknown <qrw617@N0000A062.cs.utsarr.net>
2016-10-05 12:51:00 -0500

    first commit

--- qrw617-lab2/src/test/TestGradeBook.java
+++ qrw617-lab2/src/test/TestGradeBook.java
@@ -1,8 +1,8 @@
 package test;
 
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 import org.junit.Test;
-//import core.Grades;
 
 public class TestGradeBook {
 
@@ -14,7 +14,6 @@ public class TestGradeBook {
 	@Test
 	public void testMedianNull() {
 		core.Grades grades = new core.Grades("test", null);
-		//grades.median();
 		boolean nullError = false;
         try {
             grades.median();
@@ -23,5 +22,19 @@ public class TestGradeBook {
         }
         assertTrue("should be a NullPointerException", nullError);
 	}
+	
+	@Test
+    public void testMedian10Element() {
+        ArrayList<Integer> grades = new ArrayList<Integer>();
+        //grades.add(new Integer 90, 80);
+        core.Grades grade = new core.Grades("test", grades);
+        double average = 0;
+        for (int grade : grades) {
+            average += grade;
+        }
+        average /= grades.length;
+        assertEquals("10 element test failed", average, gb.getAverage(),
+                0.000001);
+    }
 
 }
